7427bbb116a71b458ef5fb0f738fc56757196123
Antoinette7 <antoinettenyiransengimana@gmail.com>
2024-03-27 09:27:37 +0200

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,21 @@
-import java.text.NumberFormat;
+import java.util.Date;
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
 
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-      Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-      System.out.print("principal: ");
-      int principle = scanner.nextInt();
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number %5 ==0 )
+            System.out.println("Fizz");
+        else if (number %3 ==0 )
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
 
-      System.out.print("Annual Interest Rate: ");
-      float annualInterest = scanner.nextFloat();
-
-      float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-      System.out.print("period (years): ");
-      byte years = scanner.nextByte();
-      int numberOfpayment = years * MONTHS_IN_YEAR;
-
-      Double Morgage = principle
-              *(monthlyInterest * Math.pow(1 + monthlyInterest, numberOfpayment))
-              /(Math.pow(1+monthlyInterest, numberOfpayment) -1);
-      String mortgageFormatted = NumberFormat.getCurrencyInstance().format(Morgage);
-              System.out.println("Morgage: " +mortgageFormatted);
-        }
 
     }
+}
